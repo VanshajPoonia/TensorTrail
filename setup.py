f67c61b8180ca137949af1f07e06a5e4eec95945
Vanshaj Poonia <vanshajtheunique@gmail.com>
2026-05-05 20:33:19 +0530

    build: support python 3.9

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     description="TensorTrail: an educational NumPy neural network framework built from scratch.",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    python_requires=">=3.11",
+    python_requires=">=3.9",
     install_requires=["numpy"],
 )
-
